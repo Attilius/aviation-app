dadd525d4eb5eb476308b6a218cd24302c2a2e90
Attilius <leo20@freemail.hu>
2023-04-21 20:37:52 +0200

    Added AuthenticationCardLogo.vue

---
+++ resources/js/Components/AuthenticationCardLogo.vue
@@ -0,0 +1,9 @@
+<script setup>
+import { Link } from '@inertiajs/vue3';
+</script>
+
+<template>
+    <Link class="flex justify-center" :href="'/'">
+        <img class="h-15 w-15"  src="/img/logos/lorem-logo.png" alt="lorem airlines logo">
+    </Link>
+</template>
