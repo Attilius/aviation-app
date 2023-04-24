80e751e43118f9f5f600bed54556db31e67d46fe
Attilius <leo20@freemail.hu>
2023-04-24 22:02:12 +0200

    Modified tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,34 +7,34 @@ module.exports = {
         './vendor/laravel/jetstream/**/*.blade.php',
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
+        './resources/js/**/*.js',
         './resources/js/**/*.vue',
     ],
 
     theme: {
-        extend: {
-            fontFamily: {
-                sans: ['Poppins', ...defaultTheme.fontFamily.sans],
-            },
-            backgroundImage: {
-                'auth': "url('/img/backgrounds/auth-bg.jpg')",
-                'profileHeader': "url('/img/profile-header.jpg')",
-                'welcomeHeader': "url('/img/city.jpg')"
-            },
-            colors: {
-                custom_blue: '#093773',
-                lightskyblue: '#87cefa',
-                info: '#11cdef',
-                dark: '#344767',
-                rebecca_purple: '#663399',
-                light_purple: '#9d65d0',
-                whitesmoke: '#f5f5f5',
-                transparent: 'transparent',
-                black: '#000',
-                white: '#fff',
-                red: '#f00',
-                danger: '#DC2626',
-                baseGray: '#9c9c9c',
-            },
+        extend: {},
+        colors: {
+            'custom_blue': '#093773',
+            'lightskyblue': '#87cefa',
+            'info': '#11cdef',
+            'dark': '#344767',
+            'rebecca_purple': '#663399',
+            'light_purple': '#9d65d0',
+            'whitesmoke': '#f5f5f5',
+            'transparent': 'transparent',
+            'black': '#000',
+            'white': '#fff',
+            'red': '#f00',
+            'danger': '#DC2626',
+            'baseGray': '#9c9c9c',
+        },
+        fontFamily: {
+            sans: ['Poppins', ...defaultTheme.fontFamily.sans],
+        },
+        backgroundImage: {
+            'auth': "url('/img/backgrounds/auth-bg.jpg')",
+            'profileHeader': "url('/img/profile-header.jpg')",
+            'welcomeHeader': "url('/img/city.jpg')"
         },
     },
 
