97a1d04b2235cfce47a8a46f41fe03d8a96416e3
Attilius <leo20@freemail.hu>
2023-04-24 22:01:06 +0200

    Modified jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,14 @@
 module.exports = {
+    collectCoverage: true,
+    reporters: [
+        "default",
+        [
+            "./node_modules/jest-html-reporter",
+            {
+                "pageTitle": "Test Report"
+            }
+        ]
+    ],
     testRegex: [
         'resources/js/tests/.*.spec.js$',
         'resources/js/tests/.*.test.js$',
