a6200e78e765769cefd740651ea04a6f9fa3f305
Attilius <leo20@freemail.hu>
2023-04-25 16:26:52 +0200

    Modified ArrayToStingTransformer.js

--- resources/js/Tools/ArrayToStringTransformer.js
+++ resources/js/Tools/ArrayToStringTransformer.js
@@ -1,16 +1,27 @@
 export default class ArrayToStringTransformer {
 
-    constructor(array){
+    constructor(array = []) {
         this.array = array;
     }
 
-    transform(){
+    transform() {
         let transformedResult = '';
 
-        if(Array.isArray(this.array)) {
+        if(this._arrayContentValidator(this.array)) {
             this.array.map(item => transformedResult += ' ' + item);
         }
 
         return transformedResult.trim();
+    }
+
+    _arrayContentValidator(array) {
+        let validResult = false;
+
+        if(Array.isArray(array) && array.length > 0) {
+            for (let i = 0; i < array.length; i++) {
+                typeof array[1] === 'string' ? validResult = true : validResult;
+            }
+        }
+        return validResult;
     }
 }
