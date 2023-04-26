e622a373727c55a4b41675ff0f22766538c23afb
Attilius <leo20@freemail.hu>
2023-04-26 16:04:00 +0200

    Modified InputAttributesBuilder.js

--- resources/js/Tools/InputAttributesBuilder.js
+++ resources/js/Tools/InputAttributesBuilder.js
@@ -20,10 +20,25 @@ export default class InputAttributesBuilder {
     build() {
         let materialUiInputAttributes = {};
 
-        if(this.formServiceName !== '') {
+        if(this.formServiceName !== '' && typeof this.formServiceName === 'string'
+            && Object.keys(this._formServices).includes(this.formServiceName)) {
+
             this._getServiceParameters().forEach((item) => {
                 materialUiInputAttributes[item] = this._inputAttributes[item];
             });
+
+        } else {
+            if (this.formServiceName === '') {
+                throw new Error('Missing argument (formServiceName: )');
+            }
+
+            if (typeof this.formServiceName !== 'string') {
+                throw new Error('Wrong type of argument (formServiceName: )');
+            }
+
+            if (!Object.keys(this._formServices).includes(this.formServiceName)) {
+                throw new Error(`Argument ('${this.formServiceName}') does not exist`);
+            }
         }
 
         return materialUiInputAttributes;
