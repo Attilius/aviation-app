126ba2f55e89786b5c1aea660d360dc1968fa662
Attilius <leo20@freemail.hu>
2023-04-27 15:11:47 +0200

    Modified arrayToStringTransformer.spec.js

--- resources/js/tests/arrayToStingTransformer.spec.js
+++ resources/js/tests/arrayToStingTransformer.spec.js
@@ -1,9 +1,5 @@
 import ArrayToStringTransformer from '../Tools/ArrayToStringTransformer';
 
-const testArray = ['class-1', 'class-2'];
-const testObject = {key: 'value'};
-const testAssocArray = ['class-test', 13, {class: 'class-13'}]
-
 describe("Array to string transformer tests", () => {
     let transformer;
 
@@ -30,19 +26,33 @@ describe("Array to string transformer tests", () => {
     });
 
     test("Transform method returns items of array chained in a string", () => {
-        transformer = new ArrayToStringTransformer(testArray);
+        const arrayOfCorrectContents = ['class-1', 'class-2'];
+
+        transformer = new ArrayToStringTransformer(arrayOfCorrectContents);
         expect(transformer.transform()).toBe("class-1 class-2");
     });
 
     // Test cases for exceptional situations
 
-    test("Transform method returns empty string if the type of parameter is not an array", () => {
-        transformer = new ArrayToStringTransformer(testObject);
-        expect(transformer.transform()).toBe("");
-    });
+    test("Transform method returns empty string if the type of parameter is not an array or " +
+        "the content of array is not (only) string", () => {
+        const arrayOfInvalidContents = [
+            'first',
+            13,
+            3.14,
+            true,
+            null,
+            NaN,
+            {key: 'value'},
+            ['first', 13, {class: 'class-13'}, 'last'],
+            [['test']],
+            () => {return 'test-class'},
+            [false]
+        ];
 
-    test("Transform method returns empty string if the parameter is an associative array", () => {
-        transformer = new ArrayToStringTransformer(testAssocArray);
+        arrayOfInvalidContents.forEach((item) => {
+            transformer = new ArrayToStringTransformer(item);
+        });
         expect(transformer.transform()).toBe("");
     });
 });
