033ba1ed3e22e3f02e70fd2e69a81c56699f85da
Attilius <leo20@freemail.hu>
2023-05-04 20:38:21 +0200

    Added selectOption.js

---
+++ resources/js/Config/selectOption.js
@@ -0,0 +1,26 @@
+const travelTypes = {
+    first: {
+        value: 'one way',
+        text: 'One way'
+    },
+    second: {
+        value: 'round trip',
+        text: 'Round trip'
+    },
+};
+
+const cabinClasses = {
+    first: {
+        value: 'economy',
+        text: 'Economy'
+    },
+    second: {
+        value: 'business',
+        text: 'Business'
+    },
+};
+
+export {
+    travelTypes,
+    cabinClasses
+}
