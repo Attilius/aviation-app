dcc377636d0e893b510497b02d7aaab9df132887
Attilius <leo20@freemail.hu>
2023-05-04 20:39:25 +0200

    Added Welcome.vue

---
+++ resources/js/Pages/Welcome.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { Head } from '@inertiajs/vue3';
+import AppLayout from '../Layouts/AppLayout.vue';
+import Welcome from '../Components/Welcome.vue';
+//import HomeContent from './Content/HomeContent.vue';
+
+defineProps({
+    canLogin: Boolean,
+    canRegister: Boolean,
+    laravelVersion: String,
+    phpVersion: String,
+});
+</script>
+
+<template>
+    <Head>
+        <title>Home</title>
+    </Head>
+
+    <AppLayout :canLogin="canLogin" :canRegister="canRegister" title="Home">
+        <div class="">
+            <Welcome />
+           <!-- <HomeContent /> -->
+        </div>
+    </AppLayout>
+</template>
