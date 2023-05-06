3c9d7b64fe109cbc5a276a37c3a6dcb51fcd62fc
Attilius <leo20@freemail.hu>
2023-05-06 19:18:29 +0200

    Added AppLayout.vue

--- resources/js/Layouts/AppLayout.vue
+++ resources/js/Layouts/AppLayout.vue
@@ -97,6 +97,7 @@ const logout = () => {
                                         <div class="w-60">
 
                                             <!-- Team Management -->
+
                                             <template v-if="$page.props.jetstream.hasTeamFeatures">
                                                 <div class="block px-4 py-2 text-xs text-gray-400">
                                                     Manage Team
