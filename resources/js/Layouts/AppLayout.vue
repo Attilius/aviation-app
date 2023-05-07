2fa3e6c74151774839df4d07bf3f6c3872ad06e6
Attilius <leo20@freemail.hu>
2023-05-07 19:21:40 +0200

    Added AppLayout.vue

--- resources/js/Layouts/AppLayout.vue
+++ resources/js/Layouts/AppLayout.vue
@@ -173,6 +173,7 @@ const logout = () => {
                                         </template>
 
                                         <template #content>
+
                                             <!-- Account Management -->
                                             <div class="block px-4 py-2 text-xs text-gray-400">
                                                 Manage Account
