a96736b63353ce975f28347f6a9424918a2c8a2b
Attilius <leo20@freemail.hu>
2023-05-10 19:42:13 +0200

    Modified BookingForm.vue

--- resources/js/Pages/Booking/BookingForm.vue
+++ resources/js/Pages/Booking/BookingForm.vue
@@ -76,6 +76,7 @@ const send = () => {
             </div>
 
             <!-- Arriving -->
+
             <div class="flex col-span-5 sm:col-span-5">
                 <i class="material-icons prefix mr-1 relative top-3 text-2xl">flight_land</i>
                 <MaterialUiInput
