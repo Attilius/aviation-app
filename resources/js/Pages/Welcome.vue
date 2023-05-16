3048e251f3fb9a86cd787483dbb2d5464d4a616d
Attilius <leo20@freemail.hu>
2023-05-16 14:12:16 +0200

    Modified Welcome.vue

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -17,42 +17,44 @@ defineProps({
         <title>Welcome</title>
     </Head>
     <div class="max-h-screen hw-100 flex items-center bg-welcome bg-cover bg-center">
-        <div class="max-h-screen">
-            <nav class="bg-transparent fixed top-0 left-0 right-0 z-20">
+        <div class="skin">
+            <div class="max-h-screen">
+                <nav class="bg-transparent fixed top-0 left-0 right-0 z-20">
 
-                <!-- Primary Navigation Menu -->
+                    <!-- Primary Navigation Menu -->
 
-                <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-                    <div class="flex justify-between h-16">
+                    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+                        <div class="flex justify-between h-16">
 
-                        <!-- Logo -->
+                            <!-- Logo -->
 
-                        <div class="shrink-0 flex items-center">
-                            <ApplicationLogo class="block h-12 w-auto" />
-                        </div>
+                            <div class="shrink-0 flex items-center">
+                                <ApplicationLogo class="block h-12 w-auto" />
+                            </div>
 
-                        <div class="flex">
+                            <div class="flex">
 
-                            <!-- Navigation Links -->
+                                <!-- Navigation Links -->
 
-                            <div v-for="value in authNavLinkAttributes"
-                                 class="text-whitesmoke sm:-my-px sm:ml-0 sm:flex menu-item"
-                            >
-                                <NavLink :href="value.href" :active="value.active">
-                                    {{ value.name }}
-                                </NavLink>
+                                <div v-for="value in authNavLinkAttributes"
+                                     class="text-whitesmoke sm:-my-px sm:ml-0 sm:flex menu-item"
+                                >
+                                    <NavLink :href="value.href" :active="value.active">
+                                        {{ value.name }}
+                                    </NavLink>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
-            </nav>
-        </div>
-        <div>
-            <h2 class="text-whitesmoke lg:text-9xl sm:text-3xl ml-40 sm:text-center">Welcome</h2>
-            <h3 class="text-whitesmoke lg:text-6xl lg:pl-80 sm:pl-20 mb-40">
-                <span class="text-rebecca_purple font-medium">to</span>
-                Lorem Airlines
-            </h3>
+                </nav>
+            </div>
+            <div>
+                <h2 class="text-whitesmoke lg:text-9xl md:text-6xl sm:text-3xl">~Welcome~</h2>
+                <h3 class="text-whitesmoke lg:text-6xl md:text-3xl sm:text-xl mb-40 text-center">
+                    <span class="text-custom_blue font-medium">to</span>
+                    Lorem Airlines
+                </h3>
+            </div>
         </div>
     </div>
 
