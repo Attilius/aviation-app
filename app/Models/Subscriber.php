bcb7010ca13a08f18f7b75dcbb45e83508d59887
Attilius <leo20@freemail.hu>
2023-05-19 21:49:13 +0200

    Modified Subscriber.php

--- app/Models/Subscriber.php
+++ app/Models/Subscriber.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use App\Mail\MustSendEmail;
+use App\Mail\Subscribers\CanSendNewsletter;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Notifications\Notifiable;
@@ -12,6 +13,7 @@ class Subscriber extends Model
     use HasFactory;
     use Notifiable;
     use MustSendEmail;
+    use CanSendNewsletter;
 
     /**
      * The attributes that are mass assignable.
