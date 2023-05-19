15cb585c073548f1511cc2a6fb2f6805ef2f4b9d
Attilius <leo20@freemail.hu>
2023-05-19 21:49:44 +0200

    Modified SubscriberController.php

--- app/Http/Controllers/SubscriberController.php
+++ app/Http/Controllers/SubscriberController.php
@@ -2,9 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Contracts\FailedSubscribeResponseInterface;
+use App\Contracts\SubscriberBrokerInterface;
+use App\Contracts\SuccessfulSubscribeResponseInterface;
 use App\Events\Subscribed;
+use App\Facades\Subscriber;
 use Illuminate\Http\Request;
 use App\Contracts\SubscriberRepositoryInterface;
+use Laravel\Fortify\Fortify;
 
 class SubscriberController extends Controller
 {
@@ -18,7 +23,24 @@ class SubscriberController extends Controller
 
     public function subscribe(Request $request)
     {
-        $status = event(new Subscribed($this->subscriberRepository->store($request->all())));
-        dd($status);
+        event(new Subscribed($this->subscriberRepository->store($request->all())));
+
+        $status = $this->broker()->sendGreetingEmail(
+            $request->only(Fortify::email())
+        );
+
+        return $status == Subscriber::SUBSCRIBED_EMAIL_SENT
+            ? app(SuccessfulSubscribeResponseInterface::class, ['status' => $status])
+            : app(FailedSubscribeResponseInterface::class, ['status' => $status]);
+    }
+
+    /**
+     * Get the broker to be used during password reset.
+     *
+     * @return SubscriberBrokerInterface
+     */
+    protected function broker(): SubscriberBrokerInterface
+    {
+        return Subscriber::broker(config('emailing.subscribers'));
     }
 }
