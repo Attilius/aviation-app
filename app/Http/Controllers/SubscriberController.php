a4bf1792657dbb9f25c4a7776479681b75f8c3f9
Attilius <leo20@freemail.hu>
2023-05-27 21:45:45 +0200

    Modified SubscriberController.php

--- app/Http/Controllers/SubscriberController.php
+++ app/Http/Controllers/SubscriberController.php
@@ -6,41 +6,80 @@ use App\Contracts\FailedSubscribeResponseInterface;
 use App\Contracts\SubscriberBrokerInterface;
 use App\Contracts\SuccessfulSubscribeResponseInterface;
 use App\Events\Subscribed;
-use App\Facades\Subscriber;
+use App\Facades\Subscriber as SubscriberFacade;
+use App\Http\Responses\FailedSubscribeResponse;
+use App\Http\Responses\SuccessfulSubscribeResponse;
 use Illuminate\Http\Request;
 use App\Contracts\SubscriberRepositoryInterface;
 use Laravel\Fortify\Fortify;
+use App\Models\Subscriber;
 
 class SubscriberController extends Controller
 {
 
+    /**
+     * The subscriber repository implementation.
+     *
+     * @var SubscriberRepositoryInterface
+     */
     protected SubscriberRepositoryInterface $subscriberRepository;
 
+    /**
+     * Create a subscriber repository instance.
+     *
+     * @param SubscriberRepositoryInterface $subscriberRepository
+     * @return void
+     */
     public function __construct(SubscriberRepositoryInterface $subscriberRepository)
     {
         $this->subscriberRepository = $subscriberRepository;
     }
 
-    public function subscribe(Request $request)
+    /**
+     * Send a greeting email for the new subscriber.
+     *
+     * @param Request $request
+     * @return SuccessfulSubscribeResponse|FailedSubscribeResponse
+     */
+    public function subscribe(Request $request): SuccessfulSubscribeResponse|FailedSubscribeResponse
     {
         $status = $this->broker()->sendGreetingEmail(
             $request->only(Fortify::email())
         );
 
-        //event(new Subscribed($this->subscriberRepository->store($request->all())));
+         // We'll store this subscriber if his already not exist.
+         // After storing we send a greeting email and a success response.
+         // If it's exist we respond an error message with failed response.
 
-        return $status == Subscriber::SUBSCRIBED_EMAIL_SENT
+        if($this->isSubscriberAlreadyExist($request['email'])){
+            event(new Subscribed($this->subscriberRepository->store($request->all())));
+        }
+
+        return $status == SubscriberFacade::SUBSCRIBED_EMAIL_SENT
             ? app(SuccessfulSubscribeResponseInterface::class, ['status' => $status])
             : app(FailedSubscribeResponseInterface::class, ['status' => $status]);
     }
 
     /**
-     * Get the broker to be used during password reset.
+     * Get the broker to be used during subscribing.
      *
      * @return SubscriberBrokerInterface
      */
-    protected function broker(): SubscriberBrokerInterface
+    private function broker(): SubscriberBrokerInterface
     {
-        return Subscriber::broker(config('emailing.subscribers'));
+        return SubscriberFacade::broker(config('emailing.subscribers'));
+    }
+
+    /**
+     * Get subscriber for with email address from subscribers table.
+     *
+     * @param string $email
+     * @return bool
+     */
+    private function isSubscriberAlreadyExist(string $email): bool
+    {
+        $subscriber = Subscriber::where('email', $email)->get();
+
+        return count($subscriber) == 0;
     }
 }
