01e199a2670639707b8c875c9c06eeda71abadda
Attilius <leo20@freemail.hu>
2023-05-28 20:13:31 +0200

    Added subscriber.php

---
+++ lang/en/subscriber.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Subscriber Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which
+    | match reason that are given by the subscriber broker for a
+    | subscriber. Such as successful or failed subscribe attempt.
+    |
+    */
+
+    'sent' => 'We have send an email for your email address!',
+    'subscribed' => 'This email address is already subscribed!'
+
+];
