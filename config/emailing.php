b0a5bb96b64a7b0ad5d953b40e1fce006aa9f641
Attilius <leo20@freemail.hu>
2023-06-05 19:50:43 +0200

    Added emailing.php

---
+++ config/emailing.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Subscribers Broker
+    |--------------------------------------------------------------------------
+    |
+    | Here you may specify which password broker Fortify can use when a user
+    | is resetting their password. This configured value should match one
+    | of your password brokers setup in your "auth" configuration file.
+    |
+    */
+
+    'subscribers' => 'subscribers',
+];
