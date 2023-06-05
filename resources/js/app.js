ad223e1118db4e32cee1fba8932547df36d15de2
Attilius <leo20@freemail.hu>
2023-06-05 19:47:20 +0200

    Modified app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,16 +5,23 @@ import { createApp, h } from 'vue';
 import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.m';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faStar, faQuoteRight, faQuoteLeft } from '@fortawesome/free-solid-svg-icons';
+import { faStar as faStarStroke, faStarHalfStroke } from '@fortawesome/free-regular-svg-icons'
 
 const appName = window.document.getElementsByTagName('title')[0]?.innerText || 'Laravel';
+
+library.add(faStar, faStarHalfStroke, faStarStroke, faQuoteLeft, faQuoteRight);
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
     resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
     setup({ el, App, props, plugin }) {
-        return createApp({ render: () => h(App, props) })
+        return createApp({render: () => h(App, props)})
             .use(plugin)
             .use(ZiggyVue, Ziggy)
+            .component('font-awesome-icon', FontAwesomeIcon)
             .mount(el);
     },
     progress: {
