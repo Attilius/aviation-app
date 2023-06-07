7b743b4c122b2c7fb2a66568bf78b3f1d8fa142c
Attilius <leo20@freemail.hu>
2023-06-07 18:24:38 +0200

    Added Home.vue

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { Head } from '@inertiajs/vue3';
+import AppLayout from '../Layouts/AppLayout.vue';
+import HomeHeader from '../Components/HomeHeader.vue';
+</script>
+
+<template>
+    <Head>
+        <title>Home</title>
+    </Head>
+
+    <AppLayout title="Home">
+        <div class="">
+            <HomeHeader />
+        </div>
+    </AppLayout>
+</template>
