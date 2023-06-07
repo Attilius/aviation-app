8da0c55647c161c2287d3e04194170c982991583
Attilius <leo20@freemail.hu>
2023-06-07 18:20:30 +0200

    Added About.vue

---
+++ resources/js/Pages/About.vue
@@ -0,0 +1,12 @@
+<script setup>
+import AppLayout from '../Layouts/AppLayout.vue';
+import About from '../Components/About.vue';
+</script>
+
+<template>
+    <AppLayout title="About">
+        <div>
+            <About />
+        </div>
+    </AppLayout>
+</template>
