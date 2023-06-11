fc6d12eab4f3f5a5561d651bdc5224194e0cf1e6
Attilius <leo20@freemail.hu>
2023-06-11 18:21:25 +0200

    Added ContactMessage.php

---
+++ app/Models/ContactMessage.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * @method static where(string $string, $id)
+ * @method static create(array $array)
+ */
+class ContactMessage extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'email',
+        'subject',
+        'message'
+    ];
+}
