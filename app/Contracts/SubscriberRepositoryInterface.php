f7b668dd53df6cb4022e05770f6773084db1271c
Attilius <leo20@freemail.hu>
2023-06-11 18:16:04 +0200

    Modified SubscriberRepositoryInterface.php

--- app/Contracts/SubscriberRepositoryInterface.php
+++ app/Contracts/SubscriberRepositoryInterface.php
@@ -2,51 +2,8 @@
 
 namespace App\Contracts;
 
-use Illuminate\Database\Eloquent\Collection;
-use Illuminate\Database\Eloquent\Model;
-
-interface SubscriberRepositoryInterface
+interface SubscriberRepositoryInterface extends CrudInterface
 {
-    /**
-     * Get all subscriber.
-     *
-     * @return Collection
-     */
-    public function findAll(): Collection;
-
-    /**
-     * Get only subscriber with id.
-     *
-     * @param $id
-     * @return Model
-     */
-    public function findById($id): Model;
-
-    /**
-     * Store subscriber in subscribers table.
-     *
-     * @param array $data
-     * @return Model
-     */
-    public function store(array $data): Model;
-
-    /**
-     * Update subscriber role and|or email.
-     *
-     * @param array $data
-     * @return void
-     */
-    public function update(array $data = []): void;
-
-    /**
-     * Remove subscriber.
-     *
-     * @param $id
-     * @param string $email
-     * @return void
-     */
-    public function destroy($id, string $email): void;
-
     /**
      * Check if subscriber existing.
      *
