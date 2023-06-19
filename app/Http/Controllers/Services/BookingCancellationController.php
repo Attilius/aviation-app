3e3de4b7cfdd97f8ccbea804137b95e19fbdef17
Attilius <leo20@freemail.hu>
2023-06-19 21:25:55 +0200

    Added BookingCancellationController.php

---
+++ app/Http/Controllers/Services/BookingCancellationController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers\Services;
+
+use App\Http\Controllers\Controller;
+use App\Models\Service;
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+use Inertia\Response;
+
+class BookingCancellationController extends Controller
+{
+    /**
+     * Service page render.
+     *
+     * @param Request $request
+     * @return Response
+     */
+    public function index(Request $request): Response
+    {
+        $secondPartOfUri = explode('/', $request->getRequestUri())[2];
+        $service = Service::GET($secondPartOfUri);
+
+        return Inertia::render('Services/BookingCancellation', [
+            'title' => $service->title,
+            'headerImageTitles' => $service->headerImageTitles,
+            'styles' => $service->styles,
+            'description' => $service->description
+        ]);
+    }
+}
