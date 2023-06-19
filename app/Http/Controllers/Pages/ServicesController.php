02347021a31dcf52881ce1f6375ae94da859f339
Attilius <leo20@freemail.hu>
2023-06-19 21:24:01 +0200

    Added ServiceController.php

---
+++ app/Http/Controllers/Pages/ServicesController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers\Pages;
+
+use App\Http\Controllers\Controller;
+use App\Models\Page;
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+use Inertia\Response;
+
+class ServicesController extends Controller
+{
+    /**
+     * Render the service page.
+     *
+     * @param Request $request
+     * @return Response
+     */
+    public function index(Request$request): Response
+    {
+        $firstPartOfUri = explode('/', $request->getRequestUri())[1];
+        $page = Page::GET($firstPartOfUri);
+
+        return Inertia::render('Services', [
+            'title' => $page->title,
+            'cards' => $page->cards,
+            'banner' => $page->banner
+        ]);
+    }
+}
