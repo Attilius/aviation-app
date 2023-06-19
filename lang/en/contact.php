f91e1f866a3f628d098c1912f25e2072ec1d1908
Attilius <leo20@freemail.hu>
2023-06-19 21:18:34 +0200

    Added contact.php

---
+++ lang/en/contact.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Contact Message Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which
+    | match reason that are given by the subscriber broker for a
+    | subscriber. Such as successful or failed subscribe attempt.
+    |
+    */
+
+    'stored' => 'Thank you for your message. We will be in touch with our response soon.'
+];
