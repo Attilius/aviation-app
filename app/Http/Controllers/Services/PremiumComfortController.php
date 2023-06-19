7f7229f5464196a876e87226928b44c8d742ae41
Attilius <leo20@freemail.hu>
2023-06-19 21:22:44 +0200

    Added PremiumComfortController.php

---
+++ app/Http/Controllers/Services/PremiumComfortController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers\Services;
+
+use App\Http\Controllers\Controller;
+use App\Models\Service;
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+use Inertia\Response;
+
+class PremiumComfortController extends Controller
+{
+    /**
+     * Service page render.
+     *
+     * @param Request $request
+     * @return Response
+     */
+    public function index(Request $request): Response
+    {
+        $secondPartOfUri = explode('/', $request->getRequestUri())[2];
+        $service = Service::GET($secondPartOfUri);
+
+        return Inertia::render('Services/PremiumComfort', [
+            'title' => $service->title,
+            'headerImageTitles' => $service->headerImageTitles,
+            'styles' => $service->styles,
+            'description' => $service->description,
+            'categories' => $service->categories
+        ]);
+    }
+}
