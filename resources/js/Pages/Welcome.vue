a58ca1ff008a57b0d63ce8e08b4159fc8714c480
Attilius <leo20@freemail.hu>
2023-06-23 20:40:29 +0200

    Modified Welcome.vue

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -2,13 +2,18 @@
 import { Head, useForm } from '@inertiajs/vue3';
 import ApplicationLogo from '../Components/ApplicationLogo.vue';
 import NavLink from '../Components/NavLink.vue';
-import { authNavLinkAttributes }  from '../Config/navigation.js';
 import InputError from '../Components/InputError.vue';
 import MaterialUiInput from '../Components/MaterialUiInput.vue';
 import InputAttributesBuilder from '../Utils/InputAttributesBuilder.js';
 import PrimaryButton from '../Components/PrimaryButton.vue';
 
 defineProps({
+    title: String,
+    greeting: String,
+    binderWord: String,
+    companyName: String,
+    authNavLinkAttributes: Array,
+    subscriberMessage: String,
     status: String,
 });
 
@@ -28,11 +33,11 @@ const submit = () => {
 <template>
 
     <Head>
-        <title>Welcome</title>
+        <title>{{ title }}</title>
     </Head>
 
     <div class="max-h-screen hw-100 flex items-center bg-welcome bg-cover bg-center">
-        <div class="skin">
+        <div class="w-full h-screen flex-col justify-center items-center bg-shadow-skin">
             <div class="max-h-screen">
                 <nav class="bg-transparent fixed top-0 left-0 right-0 z-20">
 
@@ -63,53 +68,63 @@ const submit = () => {
                     </div>
                 </nav>
             </div>
-            <div>
-                <h2 class="text-whitesmoke lg:text-9xl md:text-6xl sm:text-3xl">~Welcome~</h2>
-                <h3 class="text-whitesmoke lg:text-6xl md:text-3xl sm:text-xl mb-40 text-center">
-                    <span class="text-lightskyblue font-medium">to</span>
-                    Lorem Airlines
+            <div class="flex-col justify-center items-center relative top-1/3">
+                <h2 class="text-whitesmoke text-3xl lg:text-6xl xl:text-9xl text-center"
+                >
+                    {{ greeting }}
+                </h2>
+                <h3 class="text-whitesmoke text-lg sm:text-3xl lg:text-6xl text-center mb-5 md:mb-16"
+                >
+                    <span class="text-lightskyblue font-medium text-sm md:text-5xl">{{ binderWord }}</span>
+                    {{ companyName }}
                 </h3>
-            </div>
 
                 <!-- Newsletter -->
 
-            <p class="text-whitesmoke lg:text-sm xs:text-xs">
-                Subscribe to our newsletter to receive our latest offers
-            </p>
+                <p class="text-whitesmoke nano-text sm:text-xs lg:text-sm text-center mb-2">
+                    {{ subscriberMessage }}
+                </p>
 
-            <form @submit.prevent="submit">
-                <div  class="flex flex-col w-full sm:max-w-md px-6 py-4 shadow-md overflow-hidden sm:rounded-lg
-                        auth-card-bg">
+                <form @submit.prevent="submit">
+                    <div class="w-4/5.2 sm:w-full flex-col max-w-md px-6 py-4 shadow-md overflow-hidden rounded-lg
+                        auth-card-bg mx-auto">
 
-                    <div v-if="status" class="mb-4 font-medium text-sm text-success text-center">
-                        {{ status }}
-                    </div>
-
-                    <div class="flex">
-                        <MaterialUiInput
-                            :id="inputAttributes.email.id"
-                            v-model="form.email"
-                            :type="inputAttributes.email.type"
-                            class="mt-1 block w-full"
-                            :customClasses="inputAttributes.email.customClasses"
-                            :label="inputAttributes.email.label"
-                            :status="status"
-                        />
-
-                        <PrimaryButton class="ml-4"
-                                       :class="{ 'opacity-25': form.processing }"
-                                       :disabled="form.processing"
+                        <div v-if="status" class="mb-4 font-medium text-xs sm:text-sm text-success text-center"
                         >
-                            Subscribe
-                        </PrimaryButton>
+                            {{ status }}
+                        </div>
+
+                        <div class="xs:flex-col sm:flex">
+                            <MaterialUiInput
+                                :id="inputAttributes.email.id"
+                                v-model="form.email"
+                                :type="inputAttributes.email.type"
+                                class="mt-1 block w-full"
+                                :customClasses="inputAttributes.email.customClasses"
+                                :label="inputAttributes.email.label"
+                                :status="status"
+                            />
+
+                            <div class="flex justify-center">
+                                <PrimaryButton class="mt-5 sm:ml-4 sm:mt-0"
+                                               :class="{ 'opacity-25': form.processing }"
+                                               :disabled="form.processing"
+                                >
+                                    Subscribe
+                                </PrimaryButton>
+                            </div>
+
+                        </div>
+
+                        <InputError class="mt-2" :message="form.errors.email" />
+
                     </div>
+                </form>
 
-                    <InputError class="mt-2" :message="form.errors.email" />
-
-                </div>
-            </form>
+            </div>
 
         </div>
     </div>
 
 </template>
+
