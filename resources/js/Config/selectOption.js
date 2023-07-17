0471b42b3a82d4eb21a022da0d47772475bdfcdc
Attilius <leo20@freemail.hu>
2023-07-17 19:47:51 +0200

    Modified selectOption.js

--- resources/js/Config/selectOption.js
+++ resources/js/Config/selectOption.js
@@ -20,7 +20,31 @@ const cabinClasses = {
     },
 };
 
+const passengers = {
+    first: {
+        value: 'Infant (0 - 23 months)',
+        text: 'Infant (0 - 23 months)'
+    },
+    second: {
+        value: 'Child (2 - 11 years)',
+        text: 'Child (2 - 11 years)'
+    },
+    third: {
+        value: 'Youth (12 - 17 years)',
+        text: 'Youth (12 - 17 years)'
+    },
+    fourth: {
+        value: 'Adult',
+        text: 'Adult'
+    },
+    fifth: {
+        value: 'Senior (65 years and Older)',
+        text: 'Senior (65 years and Older)'
+    },
+};
+
 export {
     travelTypes,
-    cabinClasses
+    cabinClasses,
+    passengers
 }
