511e37fc4f4effaad108d4600506955d246a5cdd
Attilius <leo20@freemail.hu>
2023-07-18 22:04:35 +0200

    Added KeyGeneratorInterface.php

---
+++ app/Contracts/KeyGeneratorInterface.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Contracts;
+
+interface KeyGeneratorInterface
+{
+    /**
+     * Generate unique key with different length.
+     *
+     * @param int $keyLength
+     * @return string
+     */
+    public static function generate(int $keyLength = 2): string;
+}
