51587fafa015887434da15690d32593430b5faf5
Attilius <leo20@freemail.hu>
2023-07-18 21:57:20 +0200

    Added Reservation.php

---
+++ app/Models/Reservation.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+/**
+ * @method static find(int $id)
+ */
+class Reservation extends Model
+{
+    public $timestamps = false;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'reservation_number',
+        'date_of_reservation',
+    ];
+
+    /**
+     * Get the reservation utils associated with the reservation.
+     */
+    public function reservationUtils(): HasOne
+    {
+        return $this->hasOne(ReservationUtils::class);
+    }
+
+    /**
+     * Get the flight cost associated with the reservation.
+     */
+    public function flightCost(): HasOne
+    {
+        return $this->hasOne(FlightCost::class);
+    }
+
+    /**
+     * Get the payment status associated with the reservation.
+     */
+    public function paymentStatus(): HasOne
+    {
+        return $this->hasOne(PaymentStatus::class);
+    }
+
+    /**
+     * Get the flight details that owns the reservation.
+     */
+    public function flightDetails(): BelongsTo
+    {
+        return $this->belongsTo(FlightDetails::class);
+    }
+
+    /**
+     * The passengers that belong to the reservation.
+     */
+    public function passengers(): BelongsToMany
+    {
+        return $this->belongsToMany(Passenger::class);
+    }
+}
