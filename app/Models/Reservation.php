326ffd737fd904016a20d0d174bdd101eae17cfa
Attilius <leo20@freemail.hu>
2023-07-24 20:57:20 +0200

    Modified Reservation.php

--- app/Models/Reservation.php
+++ app/Models/Reservation.php
@@ -15,11 +15,33 @@ class Reservation extends Model
     public $timestamps = false;
 
     /**
+     * The primary key associated with the table.
+     *
+     * @var string
+     */
+    public $primaryKey = 'id';
+
+    /**
+     * Indicates if the model's ID is auto-incrementing.
+     *
+     * @var bool
+     */
+    public $incrementing = false;
+
+    /**
+     * The data type of the auto-incrementing ID.
+     *
+     * @var string
+     */
+    protected $keyType = 'string';
+
+    /**
      * The attributes that are mass assignable.
      *
      * @var array<int, string>
      */
     protected $fillable = [
+        'id',
         'reservation_number',
         'date_of_reservation',
     ];
