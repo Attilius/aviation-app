8f823c669cb6cb9592b15cd2159a0b858d360660
Attilius <leo20@freemail.hu>
2023-07-24 20:57:01 +0200

    Modified PrivateJetRentController.php services

--- app/Http/Controllers/Services/PrivateJetRentController.php
+++ app/Http/Controllers/Services/PrivateJetRentController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Services;
 
 use App\Http\Controllers\Controller;
+use App\Models\Reservation;
 use App\Models\Service;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
