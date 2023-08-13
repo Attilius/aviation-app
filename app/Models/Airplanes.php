96c9a751772b2b99b7765fb2b34abbfabb609f9b
Attilius <leo20@freemail.hu>
2023-08-13 13:20:38 +0200

    Modified Airplanes.php

--- app/Models/Airplanes.php
+++ app/Models/Airplanes.php
@@ -16,13 +16,33 @@ class Airplanes
     public static function ALL(): array
     {
         $jsonData = File::get(base_path(self::JSON_FILE_PATH));
-        $airports = array();
+        $airplanes = array();
 
         foreach (json_decode($jsonData) as $value) {
-            $airports[] = $value;
+            $airplanes[] = $value;
         }
 
-        return $airports;
+        return $airplanes;
+    }
+
+    /**
+     * Return all airplanes which is inside range of distance and passenger capacity.
+     *
+     * @param string $type
+     * @return object
+     */
+    public static function findByType(string $type): object
+    {
+        $jsonData = File::get(base_path(self::JSON_FILE_PATH));
+        $airplanes = '';
+
+        foreach (json_decode($jsonData) as $value) {
+            if($value->name == $type) {
+                $airplanes = $value;
+            }
+        }
+
+        return $airplanes;
     }
 
     /**
@@ -35,14 +55,14 @@ class Airplanes
     public static function findByDistanceAndPassengers(Int $distance, Int $passengers): array
     {
         $jsonData = File::get(base_path(self::JSON_FILE_PATH));
-        $airports = array();
+        $airplanes = array();
 
         foreach (json_decode($jsonData) as $value) {
             if($value->range_distance >= $distance && $value->passenger_capacity >= $passengers) {
-                $airports[] = $value;
+                $airplanes[] = $value;
             }
         }
 
-        return $airports;
+        return $airplanes;
     }
 }
