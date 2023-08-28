8e1e053710c85e082bd2388354e79550395ce2a7
Attilius <leo20@freemail.hu>
2023-08-28 11:46:30 +0200

    Added ReservationCost.php

---
+++ app/Models/ReservationCost.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+/**
+ * @method static where(string $string, string $getId)
+ */
+class ReservationCost extends Model
+{
+    public $timestamps = false;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'item_name',
+        'price'
+    ];
+
+    /**
+     * Get the post that owns the comment.
+     */
+    public function reservation(): BelongsTo
+    {
+        return $this->belongsTo(Reservation::class);
+    }
+}
