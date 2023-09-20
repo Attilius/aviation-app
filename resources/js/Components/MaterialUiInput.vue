fb61135f129dfaf933f879d655c9fee562558351
Attilius <leo20@freemail.hu>
2023-09-20 15:43:41 +0200

    Modified MaterialUiInput.vue

--- resources/js/Components/MaterialUiInput.vue
+++ resources/js/Components/MaterialUiInput.vue
@@ -1,24 +1,35 @@
-<script setup xmlns="http://www.w3.org/1999/html">
+<script setup>
 import { onMounted, ref } from 'vue';
-import ArrayToStringTransformer from '../Utils/ArrayToStringTransformer.js';
+import { CssClassModifier } from "../Utils/CssClassModifier.js";
+import InputErrorLabelSetter from '../Utils/InputErrorLabelSetter.js';
+import InputLabelPositionAndSizeSetter from '../Utils/InputLabelPositionAndSizeSetter.js';
 
-defineProps({
+const props = defineProps({
     modelValue: String,
     id: String,
     type: String,
     label: Object,
     customClasses: Array,
+    error: String,
+    inputValueStatus: String,
+    _for_: String
 });
 
-defineEmits(['update:modelValue']);
-
+const emit = defineEmits(['update:modelValue', 'updateInputError']);
 const input = ref(null);
-
-let inputBaseCssClasses = `relative h-8 w-full border-t-0 border-l-0 border-r-0 input-border-outline
-    bg-transparent focus:ring-transparent transition-colors z-10 peer duration-500`;
+let inputBaseCssClasses = `relative h-8 w-full border-none input-border-outline p-0 bg-transparent
+    focus:ring-transparent transition-colors z-10 peer`;
 
 let labelBaseCssClasses = `absolute left-2 px-1 -top-0.5 text-md cursor-text peer-focus:text-xs peer-focus:-top-4
     transition-all duration-500`;
+
+const cssClassModifier = new CssClassModifier();
+const inputErrorSetter = new InputErrorLabelSetter();
+
+inputErrorSetter.set(props, 'departure_from');
+inputErrorSetter.set(props, 'arriving_at');
+inputErrorSetter.set(props, 'departure_date');
+inputErrorSetter.set(props, 'return_date');
 
 onMounted(() => {
     if (input.value.hasAttribute('autofocus')) {
@@ -31,30 +42,9 @@ onMounted(() => {
  * Set the position and size of the label when the associated input field contains a value.
  */
 const setLabelPositionAndSize = () => {
-    const labels = document.getElementsByTagName('label');
-    const inputs = document.getElementsByTagName('input');
+    const labelPositionSetter = new InputLabelPositionAndSizeSetter();
 
-    for (let input_item of inputs) {
-        for (let label_item of labels) {
-            if (input_item.value && input_item.id === label_item.htmlFor) {
-                label_item.classList.replace('text-md', 'text-xs');
-                label_item.classList.replace('-top-0.5', '-top-4');
-            } else if (!input_item.value && input_item.id === label_item.htmlFor) {
-                label_item.classList.replace('text-xs', 'text-md');
-                label_item.classList.replace('-top-4', '-top-0.5');
-            }
-        }
-    }
-}
-
-const classAttributeExpander = (baseCssClasses, customClasses) => {
-
-    if (customClasses) {
-        const transformedArray = new ArrayToStringTransformer(customClasses).transform();
-        baseCssClasses += " " + transformedArray;
-    }
-
-    return baseCssClasses;
+    labelPositionSetter.set();
 }
 
 defineExpose({ focus: () => input.value.focus() });
@@ -66,19 +56,19 @@ defineExpose({ focus: () => input.value.focus() });
                :type="type"
                :id="id"
                ref="input"
-               :class="classAttributeExpander(inputBaseCssClasses, customClasses)"
-               required
+               :class="cssClassModifier.classAttributeExpander(inputBaseCssClasses, customClasses)"
                autocomplete="off"
+               required
                :value="modelValue"
-               @input="$emit('update:modelValue', $event.target.value); setLabelPositionAndSize();">
+               @input="emit('update:modelValue', $event.target.value);
+                       emit('updateInputError', ['', props.id]);
+                       setLabelPositionAndSize();"
+        >
         <label data-testId="mui-label"
-               :for="label._for_"
-               :class="classAttributeExpander(labelBaseCssClasses, label.customClasses)">
+               :id="label.id"
+               :for="_for_ ? label._for_ + _for_ : label._for_"
+               :class="cssClassModifier.classAttributeExpander(labelBaseCssClasses, label.customClasses)">
                {{ label.textValue }}
         </label>
     </div>
 </template>
-
-<style>
-
-</style>
