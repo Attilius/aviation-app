8335bea7ba9fcfec699dd56fb4685afb7a1028ba
Attilius <leo20@freemail.hu>
2023-09-21 22:36:01 +0200

    Modified PrimaryButton.vue

--- resources/js/Components/PrimaryButton.vue
+++ resources/js/Components/PrimaryButton.vue
@@ -1,5 +1,5 @@
 <script setup>
-defineProps({
+const props = defineProps({
     type: {
         type: String,
         default: 'submit',
@@ -10,10 +10,10 @@ defineProps({
 <template>
     <button data-testId="primary-button"
             :type="type"
-            class="rounded-full inline-flex items-center px-4 py-2 bg-rebecca_purple border
+            class="rounded-full items-center px-4 py-2 bg-rebecca_purple border
                    border-transparent font-semibold text-xs text-whitesmoke uppercase tracking-widest
-                   hover:bg-whitesmoke hover:text-rebecca_purple hover:border-rebecca_purple active:bg-gray-900
-                   focus:outline-none focus:border-gray-900 focus:ring focus:ring-light_purple disabled:opacity-25
+                   hover:bg-whitesmoke hover:text-rebecca_purple hover:border-rebecca_purple
+                   focus:outline-none focus:ring focus:ring-light_purple disabled:opacity-25
                    smooth-transition hover:duration-500"
     >
         <slot />
