8a99000e4f6e0d3973e184c53f266247a34c5922
Attilius <leo20@freemail.hu>
2023-09-27 20:43:42 +0200

    Added FavoritePlace.php

---
+++ app/Models/FavoritePlace.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+/**
+ * @method create(array $array)
+ */
+class FavoritePlace extends Model
+{
+    use HasFactory;
+
+    public $timestamps = false;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'city_name',
+        'service_id',
+        'service_name',
+        'service_type',
+        'service_image',
+        'service_address',
+        'service_phone'
+    ];
+
+    /**
+     * Get the user that owns the comment.
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
