c9a1b7e96dae56dfdbc42a54735821be27b5fbf0
Attilius <leo20@freemail.hu>
2023-10-01 17:21:02 +0200

    Added ResponsiveNavLink.vue

---
+++ resources/js/Components/ResponsiveNavLink.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { computed } from 'vue';
+import { Link } from '@inertiajs/vue3';
+
+const props = defineProps({
+    active: Boolean,
+    href: String,
+    as: String,
+});
+
+const classes = computed(() => {
+    return props.active
+        ? 'block w-full pl-3 pr-4 py-2 text-left text-base font-bold text-lightskyblue hover:text-whitesmoke hover:bg-rebecca_purple hover:border-white hover:border-l-8 focus:outline-none focus:text-indigo-800 focus:bg-indigo-100 focus:border-indigo-700 transition duration-150 ease-in-out'
+        : 'block w-full pl-3 pr-4 py-2 text-left text-base font-medium text-whitesmoke hover:text-whitesmoke hover:bg-rebecca_purple hover:border-white hover:border-l-8 focus:outline-none focus:text-gray-800 focus:bg-gray-50 focus:border-gray-300 transition duration-150 ease-in-out';
+});
+</script>
+
+<template>
+    <div>
+        <button v-if="as == 'button'" :class="classes" class="w-full text-left">
+            <slot />
+        </button>
+
+        <Link v-else :href="href" :class="classes">
+            <slot />
+        </Link>
+    </div>
+</template>
