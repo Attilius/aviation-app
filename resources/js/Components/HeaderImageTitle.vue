26c0334e0f73385643ed7b0398a7fbc3f043a228
Attilius <leo20@freemail.hu>
2023-10-02 20:24:43 +0200

    Added HeaderImageTitle.vue

---
+++ resources/js/Components/HeaderImageTitle.vue
@@ -0,0 +1,12 @@
+<script setup>
+defineProps({
+   title: String
+});
+</script>
+
+<template>
+    <div class="h-16 bg-white-alfa-8 pt-2 px-3 absolute top-24 left-1/20 text-custom_blue text-5xl mt-48 text-center"
+    >
+        <h3>{{ title }}</h3>
+    </div>
+</template>
