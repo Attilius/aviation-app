4aec27f50bab8ef9e558e3d6181417ac8375f0e1
Attilius <leo20@freemail.hu>
2023-10-02 20:19:39 +0200

    Added BookingButton.vue

---
+++ resources/js/Components/TravelGuide/Destination/BookingButton.vue
@@ -0,0 +1,47 @@
+<script setup>
+const props = defineProps({
+    city: Object
+});
+</script>
+
+<template>
+    <div class="box">
+        <button class="btn rounded-full capitalize">
+            <i id="rotate-90deg" class="material-icons">flight</i>
+            {{ city.name.replace('-', ' ') }}
+            <i id="rotate-90deg" class="material-icons">expand_less</i>
+        </button>
+    </div>
+</template>
+
+<style scoped>
+.box {
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+
+.btn {
+    background: rebeccapurple;
+    height: 50px;
+    width: 170px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    margin-top: 50px;
+    padding: 20px;
+}
+
+.btn:hover {
+    color: whitesmoke;
+}
+
+.btn i {
+    color: whitesmoke;
+}
+
+#rotate-90deg {
+    transform: rotate(90deg);
+}
+</style>
