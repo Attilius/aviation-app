6de56c8161f6fe7866149dde6ae9a9a453436e6f
Attilius <leo20@freemail.hu>
2023-10-02 20:23:51 +0200

    Added Header.vue

---
+++ resources/js/Components/TravelGuide/Destination/Header.vue
@@ -0,0 +1,17 @@
+<script setup>
+import HeaderFront from '../../../Components/TravelGuide/Destination/HeaderFront.vue';
+
+const props = defineProps({
+    destination: Object,
+    currentTime: String
+});
+</script>
+
+<template>
+    <div>
+        <div class="w-full h-screen bg-cover bg-no-repeat text-whitesmoke relative top-0"
+             :class="destination.header_image_css">
+            <HeaderFront :destination="destination" :currentTime="currentTime" />
+        </div>
+    </div>
+</template>
