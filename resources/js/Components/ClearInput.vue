55e646ff90793f6d11fac458e4de9ed8c65f1ee1
Attilius <leo20@freemail.hu>
2023-10-02 20:20:37 +0200

    Added ClearInput.vue

---
+++ resources/js/Components/ClearInput.vue
@@ -0,0 +1,24 @@
+<script setup>
+
+const props = defineProps({
+    error: String,
+    inputValueStatus: String,
+    modelValue: String,
+    id: String
+});
+
+const emit = defineEmits(['updateModelValue']);
+
+</script>
+
+<template>
+    <div :id="id" @click="emit('updateModelValue', ['', id])"
+         class="w-8 h-8 cursor-pointer flex justify-center items-center mt-1">
+        <font-awesome-icon id="remove-icon"
+                           :icon="['fas', 'xmark']"
+                           class="text-lg block"
+                           :class="error !== '' || inputValueStatus === '' ? 'hidden': 'block'"
+        />
+    </div>
+</template>
+
