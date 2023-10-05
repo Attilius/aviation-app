046472086fa10f98317b492142fefae51c39f5a5
Attilius <leo20@freemail.hu>
2023-10-05 10:14:21 +0200

    Added Flight.php

---
+++ app/Models/Flight.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * @method static create(string[] $array)
+ * @method static find(int $int)
+ * @method static where(string $string, $flight_details_id)
+ */
+class Flight extends Model
+{
+    use HasFactory;
+
+    public $timestamps = false;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'departure_place',
+        'arriving_place',
+        'flight_number',
+        'carrier_name',
+        'airplane_type',
+        'departure_date',
+        'arriving_date',
+        'departure_time',
+        'arriving_time',
+        'free_seats'
+    ];
+}
