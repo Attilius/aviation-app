7ae28f7cbb19463d346b94708721cda35a505fc8
Attilius <leo20@freemail.hu>
2023-10-06 21:27:15 +0200

    Modified DestinationController.php

--- app/Http/Controllers/Pages/TravelGuide/DestinationController.php
+++ app/Http/Controllers/Pages/TravelGuide/DestinationController.php
@@ -4,7 +4,9 @@ namespace App\Http\Controllers\Pages\TravelGuide;
 
 use App\Http\Controllers\Controller;
 use App\Models\Destination;
+use App\Repository\FavoritePlacesRepository;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Http;
 use Inertia\Inertia;
 use Inertia\Response;
@@ -19,6 +21,11 @@ class DestinationController extends Controller
      */
     public function index(Request $request): Response
     {
+        $user = Auth::user();
+        $repository = new FavoritePlacesRepository();
+
+        $favoritePlaces = $repository->findAllFavoritePlaceByUserId($user->id);
+
         $destination = Destination::findByCityName($request->get('city'));
         $response = Http::get("https://timeapi.io/api/Time/current/zone?timeZone=$destination->area/$destination->location");
         $destination->currentTime = strval($response->json()['time']);
@@ -26,6 +33,7 @@ class DestinationController extends Controller
         return Inertia::render('TravelGuide/Destination', [
             'destination' => $destination,
             'title' => $destination->city->name,
+            'favoritePlaces' => $favoritePlaces
         ]);
     }
 
