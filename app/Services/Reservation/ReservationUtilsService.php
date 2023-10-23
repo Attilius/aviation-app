e19b4fa49dfcbaff642fab7a8c0e7ba7264adbc9
Attilius <leo20@freemail.hu>
2023-10-23 20:24:47 +0200

    Added ReservationUnitsService.php

---
+++ app/Services/Reservation/ReservationUtilsService.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Services\Reservation;
+
+use App\Models\Reservation;
+use App\Models\ReservationUtils;
+use Illuminate\Http\Request;
+
+class ReservationUtilsService
+{
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @param string $id
+     * @param string $pax
+     * @param string $travelType
+     * @return void
+     */
+    public function createReservationUtils(string $id, string $pax, string $travelType = '*'): void
+    {
+        $reservation = Reservation::find($id);
+        $reservation->reservationUtils()->create([
+            'pax' => $pax,
+            'target_of_plane_choosing' => '*',
+            'travel_type' => $travelType
+        ]);
+    }
+
+    /**
+     * Update the reservation utils resource.
+     *
+     * @param string $id
+     * @param Request $request
+     * @return void
+     */
+    public function updateReservationUtils(Request $request, string $id): void
+    {
+        ReservationUtils::where('reservation_id', $id)->update([
+            'target_of_plane_choosing_back' => $request->get('targetOfPlaneChoosing'),
+            'return_flight_numbers' => $request->get('returnFlightNumber'),
+            'return_airplane_type' => $request->get('returnAirplaneType')
+        ]);
+    }
+}
